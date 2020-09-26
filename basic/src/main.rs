56686480d59c443341f50c638e55d2d6be6498f9
Mandar <01.mandar@gmail.com>
2020-09-26 22:55:54 +0530

    ref guards

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,3 +1,6 @@
+use std::convert::From;
+use std::fmt;
+
 #[derive(Debug)]
 struct Person<'a>
 {
@@ -8,6 +11,18 @@ struct Person<'a>
 struct Point{
     x:i32, y:i32
 } 
+
+impl From<i32> for Point{
+    fn from(item: i32) -> Self {
+        Point { x: item ,y: 0}
+    }
+}
+
+impl fmt::Display for Point{
+    fn fmt(&self ,f: &mut fmt::Formatter)->fmt::Result{
+        write!(f," point is {}",self.x)
+    }
+}
 // struct of struct
 struct Rect{
     up:Point,down:Point
@@ -19,11 +34,15 @@ enum Canvas{
     Dot(i32,i32)
 }
 
+static _IAMSTATIC: &str ="static";
+const _IAMCONST:i32=1; 
+type _UpperCamelCase =u64; // Aliasing
+
 fn draw(event:Canvas){
     match event{
-        Canvas::Whiteboard=> println!("_Whiteboard"),
-        Canvas::Dot(_,_)=> println!("_Dot"),
-        Canvas::Rect=> println!("_Rect"),
+        Canvas::Whiteboard=> println!("enum _Whiteboard"),
+        Canvas::Dot(_,_)=> println!("enum _Dot"),
+        Canvas::Rect=> println!("enum _Rect"),
     }
 }
 
@@ -35,11 +54,28 @@ fn main() {
     let age = 27;
     let peter = Person { name, age };
 
+    // stuct
     let point_obj : Point=Point {x:1,y:1};
     let point_new = Point{x:3,..point_obj};
-    println!("new point ({}, {})", point_new.x,point_new.y);
+    println!("struct new point ({}, {})", point_new.x,point_new.y);
 
+    // enum
     let idot = Canvas::Dot(3,4);
     draw(idot);
 
+    // from into
+    let num1 = Point::from(30);
+    println!("from into point ({}, {})", num1.x,num1.y);
+
+    // tostring
+    println!("tostring {}", num1.to_string());
+
+    //ref + guard
+    let value = 1;
+    match value {
+        ref r if r>=&0i32  =>println!("  0<{:?}", r),
+        _=>println!("0"),
+    }
+
+   
 }
