ee9bc742d7492f5b10498345e056df83ad391bcb
Mandar <01.mandar@gmail.com>
2020-10-01 16:36:11 +0530

    channel and file

--- stdmisc/src/main.rs
+++ stdmisc/src/main.rs
@@ -1,6 +1,46 @@
 
 use std::thread;
 
+use std::sync::mpsc::{Sender,Receiver};
+use std::sync::mpsc;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
+static count:i32=5;
+
+fn RunChannels(){
+    let (tx,rx): (Sender<i32>,Receiver<i32>)=mpsc::channel();
+
+    let mut cha_list = Vec::new();
+
+    // TX
+    for id in 0..5 {
+        let thr_tx = tx.clone();
+
+        let cha =thread::spawn(move||{
+            thr_tx.send(id).unwrap();
+            println!(" TX {}",id);
+        });
+        cha_list.push(cha);
+    }
+
+    // RX
+    let mut ids = Vec::with_capacity(count as usize);
+
+    for _ in 0..count{
+        ids.push(rx.recv());
+    }
+    
+    // WAIT
+    for cha in cha_list{
+        cha.join().expect("bad panick");
+    }
+    println!("{:?}" ,ids);
+}
+
 
 
 fn RunThread(){
@@ -18,7 +58,39 @@ fn RunThread(){
     }
 }
 
+fn MakeFile()
+{
+   // Create a path to the desired file
+   let path = Path::new("hello.txt");
+   let display = path.display();
+
+   let mut file;
+   
+   file = match File::create(&path){
+       Err(why) => panic!("create fail {}",why),
+       Ok(file) => file,
+   };
+
+   match file.write_all("hello test".as_bytes()){
+       Err(why) => panic!("write fail {}",why),
+       Ok(_)=>println!("written"),
+   }
+
+   file= match File::open(&path) {
+       Err(why) => panic!("----------- {}: {}", display, why),
+       Ok(file) => file,
+   };
+
+ 
+   let mut s = String::new();
+   match file.read_to_string(&mut s) {
+       Err(why) => panic!("-------------- {}: {}", display, why),
+       Ok(_) => print!("{} contains:\n{}", display, s),
+   }
+}
 
 fn main() {
     RunThread();
+    RunChannels();
+    MakeFile();
 }
