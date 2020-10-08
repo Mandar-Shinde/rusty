fa088ca728f737eff5646b206b0b475ba022b066
Mandar <01.mandar@gmail.com>
2020-10-08 21:31:30 +0530

    json

--- parsing/json/src/main.rs
+++ parsing/json/src/main.rs
@@ -10,6 +10,22 @@ fn printjson()
     println!("Create:\n {:#}",data);
 }
 
+fn parsejson()
+{
+    let parsed = json::parse(r#"
+    {
+        "name": "some name",
+        "age": 15,
+        "canJSON": true
+    }
+"#).unwrap();
+
+println!("Printed:\n {}",parsed);
+
+println!("Printed Pretty:\n {:#}",parsed)
+}
+
 fn main() {
     printjson();
+    parsejson();
 }
